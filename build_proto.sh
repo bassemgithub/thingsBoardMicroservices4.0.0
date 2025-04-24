01236af3e14cc6e8588f161927a2530b0e0ea04c
Bassem Khil <khilbassem@gmail.com>
2025-04-24 19:33:33 +0200

    change build proto

--- build_proto.sh
+++ build_proto.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# remvove this comment this is a change to remove
 #
 
 echo "Building ThingsBoard protobuf-containing packages..."
